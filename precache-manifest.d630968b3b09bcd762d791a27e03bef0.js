80d7201a7f2fb624d1302900bc321541ba9b52f6
yashpatel1492 <yashpatel1492002@gmail.com>
2022-09-22 00:00:17 +0530

    Updates

---
+++ precache-manifest.d630968b3b09bcd762d791a27e03bef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9301cb2239b6e0eab5067aee28e989ed",
+    "url": "/20DCS092_Yash/index.html"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/20DCS092_Yash/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "225c7e20b11912768f21",
+    "url": "/20DCS092_Yash/static/css/main.48551862.chunk.css"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/20DCS092_Yash/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/20DCS092_Yash/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225c7e20b11912768f21",
+    "url": "/20DCS092_Yash/static/js/main.fb9f4459.chunk.js"
+  },
+  {
+    "revision": "0fbebfee2b2c9719b262",
+    "url": "/20DCS092_Yash/static/js/runtime-main.61064e84.js"
+  }
+]);
